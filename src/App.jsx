b300bc355aaf3c82d072f48f33a00cce02318ca3
ccchoi7171 <ccchoi7171@gmail.com>
2025-03-21 18:02:19 +0900

    Init: 프로젝트 최초 커밋

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Tailwind 완전 적용됨 </h1>
+    </div>
+  );
+}
+
+export default App;
